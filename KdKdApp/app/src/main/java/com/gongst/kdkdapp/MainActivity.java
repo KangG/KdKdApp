3a7ca623f51a4fb64445cbb5112d34cbf6fb2505
dongmin7322 <dongmin7322@naver.com>
2019-12-17 14:08:48 +0900

    kdkdapp

---
+++ KdKdApp/app/src/main/java/com/gongst/kdkdapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.gongst.kdkdapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    private  WebView mwv;//Mobile Web View
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+        mwv=(WebView)findViewById(R.id.activity_main_webview);
+
+        WebSettings mws=mwv.getSettings();//Mobile Web Setting
+        mws.setJavaScriptEnabled(true);//자바스크립트 허용
+        mws.setLoadWithOverviewMode(true);//컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+
+        mwv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mwv.loadUrl("http://183.101.196.138:8888");
+    }
+
+    //   추가전에 뒤로가기 이벤트 호출시 홈으로 돌아갔으나, 이젠 일반적인 뒤로가기 기능 활성화
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mwv.canGoBack()) {
+                mwv.goBack();
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
+}
